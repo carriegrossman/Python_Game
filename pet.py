65f8672a49393116a7a756909f6a0b2e3deac4f9
Carrie Grossman <carriegrossman@Carries-MacBook-Pro.local>
2020-06-10 15:06:30 -0400

    modified project

--- pet.py
+++ pet.py
@@ -1,23 +1,44 @@
 class Pet():
-    def __init__(self, name):
+    def __init__(self, name, fullness=50, energy=50, purr_happiness=85, hunger=5, sad=5):
         self.name = name
-        self.hunger = 15
-        self.energy = 45
-        self.purr_happiness = 85
+        self.fullness = fullness
+        self.energy = energy
+        self.purr_happiness = purr_happiness
+        self.hunger = hunger
+        self.sad = sad
 
-    def play(self):
-        self.energy -=15
-
-    def get_cuddles(self):
+    def get_love(self):
         self.purr_happiness +=10 
     
     def eat_food(self):
-        self.energy += 10
-        self.hunger -= 20
+        self.fullness += 30
+    
+    def nap_time(self):
+        self.energy += 50
+    
+    def life(self):
+        self.fullness -= self.hunger
+        self.purr_happiness -= self.sad
+
+#subclass of class Pet
+class PlayfulCat(Pet):
+    def __init__(self, name, fullness=50, energy=50, hunger=5, play_level=1):
+        super().__init__(name, fullness, energy, 100, hunger, 1)
+        self.play_level = play_level
+    
+    def life(self):
+        self.fullness -= self.hunger
+        self.purr_happiness -= self.sad/2
+    
+    def play(self, other_cat):
+        #Super play powers
+        for i in range(self.play_level):
+            other_cat.get_love()
 
 
-dexter = Pet("Dexter")
-dexter.play()
-print(dexter.energy)
-dexter.get_cuddles()
+dexter = Pet("Dexter", 50, 50, 85, 5, 5)
+sushi = PlayfulCat("Sushi", 65, 70, 95, 3)
+
+print(dexter.purr_happiness)
+sushi.play(dexter)
 print(dexter.purr_happiness)
